00828ea9899e9024a322a8eee5c24bd48c6483b1
Aleksandr <127627433+alvi0avcc@users.noreply.github.com>
2025-06-25 12:16:18 +0300

    feat: Test version of header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,8 +1,25 @@
 import styles from './header.module.css';
+import { Wrapper } from '@/components/wrapper/wrapper';
+
 export const Header = () => {
   return (
     <header className={styles.header}>
-      <h2>Header</h2>
+      <Wrapper>
+        <label htmlFor="menu-toggle" className={styles.menuLabel}>
+          <h2>ST</h2>
+        </label>
+
+        <input type="checkbox" id="menu-toggle" className={styles.menuToggle} />
+
+        <ul className={styles.menu}>
+          <li>menu-1</li>
+          <li>menu-2</li>
+          <li>menu-3</li>
+          <li>menu-4</li>
+          <li>menu-5</li>
+        </ul>
+
+      </Wrapper>
     </header>
   );
 };
--- src/pages/home-page/home-page.tsx
+++ src/pages/home-page/home-page.tsx
@@ -1,11 +1,10 @@
-import { Wrapper } from '../../components/wrapper/wrapper';
-import { messages } from '../../sources/messages';
+import { Wrapper } from '@/components/wrapper/wrapper';
+import { messages } from '@/sources/messages';
 import styles from './home-page.module.css';
 
 export const HomePage = () => {
   return (
     <Wrapper>
-      {' '}
       <div className={styles.container}>{messages.titles.homePageTitle}</div>
     </Wrapper>
   );
