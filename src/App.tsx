421942cd9ec39eca28067f3ffb295c8e65fa3b05
Aleksandr <127627433+alvi0avcc@users.noreply.github.com>
2025-07-04 13:27:56 +0300

    feat: Logo snake

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,21 @@
+import { useState } from 'react';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router/router';
+import { SplashScreen } from './components/splash-screen/splash-screen';
 
 export const App = () => {
-  return <RouterProvider router={router} />;
+  const [showSplash, setShowSplash] = useState(true);
+
+  return (
+    <>
+      {showSplash && (
+        <SplashScreen
+          duration={2500}
+          onAnimationEnd={() => setShowSplash(false)}
+        />
+      )}
+
+      {!showSplash && <RouterProvider router={router} />}
+    </>
+  );
 };
--- src/components/logo/logo.tsx
+++ src/components/logo/logo.tsx
@@ -1,18 +1,22 @@
 import styles from './logo.module.css';
 
+import SnakeLogo from './snake.svg';
+
 type LogoProps = {
-  isMobile: boolean;
-  toggleMenu: () => void;
+  isMobile?: boolean;
+  toggleMenu?: () => void;
+  className?: string;
 };
 
-export const Logo = ({ isMobile, toggleMenu }: LogoProps) => (
-  <div className={styles.container}>
+export const Logo = ({ isMobile, toggleMenu, className }: LogoProps) => (
+  <div className={`${styles.container} ${className || ''}`}>
     <button
       className={`${styles.logo} ${isMobile ? styles.logoMobile : ''}`}
-      onClick={toggleMenu}
+      onClick={toggleMenu && toggleMenu}
       aria-label="Toggle menu"
     >
-      <span>ST</span>
+      {/* <span>ST</span> */}
+      <img src={SnakeLogo} alt="Logo StudioTeams" />
     </button>
   </div>
 );
